66e317668c0216a0ff2aabd4556f5f76b91da940
Antonio García-Domínguez <nyoescape@gmail.com>
2022-02-15 15:26:47 +0000

    Add sample code to run EGL from Java

---
+++ src/main/java/com/mdenet/egl/Project2Chart.java
@@ -0,0 +1,42 @@
+package com.mdenet.egl;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.Collections;
+import java.util.Map;
+
+import com.google.common.base.Charsets;
+
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.emfatic.core.EmfaticResourceFactory;
+import org.eclipse.epsilon.egl.EglTemplateFactoryModuleAdapter;
+import org.eclipse.epsilon.emc.emf.EmfModel;
+import org.eclipse.epsilon.flexmi.FlexmiResourceFactory;
+
+public class Project2Chart {
+    public static void main(String[] args) throws Exception {
+        // 1. Enable Flexmi and Emfatic support (for this example)
+        final Map<String, Object> emap = Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap();
+        emap.put("flexmi", new FlexmiResourceFactory());
+        emap.put("emf", new EmfaticResourceFactory());
+
+        // 2. Parse the EGL sources
+        EglTemplateFactoryModuleAdapter egl = new EglTemplateFactoryModuleAdapter();
+        egl.parse(Project2Chart.class.getResource("/egl/project2chart.egl"));
+
+        // 3. Add model
+        EmfModel model = new EmfModel();
+        model.setMetamodelFile("metamodels/project.emf");
+        model.setModelFile("models/project.flexmi");
+        model.setReadOnLoad(true);
+        model.setStoredOnDisposal(false);
+        model.load();
+        egl.getContext().getModelRepository().addModel(model);
+
+        // 4. Run the template
+        String html = (String) egl.execute();
+        Files.write(new File("output.html").toPath(),
+            Collections.singleton(html),
+            Charsets.UTF_8);
+    }
+}
