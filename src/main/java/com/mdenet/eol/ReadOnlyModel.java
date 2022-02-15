5d162368bf652d83863e6dde057b369f3a3f86b7
Antonio García-Domínguez <nyoescape@gmail.com>
2022-02-15 12:27:05 +0000

    First version of ReadOnlyModel script

---
+++ src/main/java/com/mdenet/eol/ReadOnlyModel.java
@@ -0,0 +1,40 @@
+package com.mdenet.eol;
+
+import java.util.Map;
+
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.emfatic.core.EmfaticResourceFactory;
+import org.eclipse.epsilon.emc.emf.EmfModel;
+import org.eclipse.epsilon.eol.EolModule;
+import org.eclipse.epsilon.flexmi.FlexmiResourceFactory;
+
+public class ReadOnlyModel {
+    public static void main(String[] args) throws Exception {
+        // 1. Enable Flexmi and Emfatic support (for this example)
+        final Map<String, Object> emap = Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap();
+        emap.put("flexmi", new FlexmiResourceFactory());
+        emap.put("emf", new EmfaticResourceFactory());
+
+        // 2. Parse the EOL code
+        EolModule eol = new EolModule();
+        eol.parse(ReadOnlyModel.class.getResource("/eol/02-read_only.eol"));
+        
+        // 3. Add models to the repository
+        EmfModel m = new EmfModel();
+        m.setMetamodelFile("metamodels/project.emf");
+        m.setModelFile("models/project.flexmi");
+        m.setReadOnLoad(true);
+        m.setStoredOnDisposal(false);
+        m.load();
+        eol.getContext().getModelRepository().addModel(m);
+
+        // 4. Run the script
+        try {
+            eol.execute();
+        } finally {
+            // 5. Always dispose *all* models
+            eol.getContext().getModelRepository().dispose();
+        }
+    }
+
+}
